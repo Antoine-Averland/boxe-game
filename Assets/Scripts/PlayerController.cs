1f8c879d86a15d1f9f23fe589ec60afff5827d8d
ThibaultAnd261 <thibaultandwork@gmail.com>
2024-04-28 19:52:26 +0200

    Add Kick + Punch on animations & comment move anim due to bugs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,21 +20,60 @@ public class PlayerControllerV2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Punch();
+        Kick();
         Move();
+    }
+
+    public void Punch()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Anim.Play("Punch1_R");
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Anim.Play("Punch1_L");
+        }
+    }
+
+    public void Kick()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Anim.Play("Kick1_R");
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Anim.Play("Kick1_L");
+        }
     }
 
     public void Move()
     {
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(-movementSpeed, 0, 0);
-            Anim.Play("MoveBack");
+            transform.position += new Vector3(-movementSpeed, 0, 0);
+            //Anim.Play("MoveBack");
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(movementSpeed, 0, 0);
-            Anim.Play("MoveForw");
+            transform.position += new Vector3(movementSpeed, 0, 0);
+            //Anim.Play("MoveForw");
+        }
+
+        // Vérifier si la touche W est enfoncée
+        if (Input.GetKey(KeyCode.W))
+        {
+            // Déplacer le personnage vers l'avant en fonction de la vitesse de déplacement
+            transform.position += new Vector3(0, 0, movementSpeed);
+        }
+        // Vérifier si la touche S est enfoncée
+        else if (Input.GetKey(KeyCode.S))
+        {
+            // Déplacer le personnage vers l'arrière en fonction de la vitesse de déplacement
+            transform.position += new Vector3(0, 0, -movementSpeed);
         }
 
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
