8f534a703b85c46054d6006ca48b0d7ebac2c2a7
Antoine Averland <antoine.averland@edu.esiee.fr>
2025-05-31 13:44:32 +0200

    Add haptic vibrations of controllers on hit, but not tested because still head position bug

--- Assets/Scripts/VrHandDamage.cs
+++ Assets/Scripts/VrHandDamage.cs
@@ -1,23 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class VRHandDamage : MonoBehaviour
 {
+    public DamageController damageController;
     public DamageController.AttackType attackType = DamageController.AttackType.Punch;
-    public DamageController.AttackSide attackSide = DamageController.AttackSide.Left;
+    public DamageController.AttackSide attackSide = DamageController.AttackSide.Right; // à définir par main
+    public XRNode handNode = XRNode.RightHand; // Choisir LeftHand ou RightHand dans l’inspecteur
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("BoxerBlueSide"))
         {
-            DamageController dc = other.GetComponent<DamageController>();
-            if (dc != null)
-            {
-                dc.SetLastAttack(attackType);
-                dc.SetLastAttackSide(attackSide);
-                dc.SetCanDamage(true);
-            }
+            // Définir les infos d’attaque
+            damageController.SetLastAttack(attackType);
+            damageController.SetLastAttackSide(attackSide);
+            damageController.SetCanDamage(true);
+
+            // Déclencher vibration
+            TriggerHapticFeedback(0.8f, 0.5f); // amplitude (0-1), durée en secondes
+        }
+    }
+
+    private void TriggerHapticFeedback(float amplitude, float duration)
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(handNode);
+
+        if (device.TryGetHapticCapabilities(out HapticCapabilities capabilities) && capabilities.supportsImpulse)
+        {
+            device.SendHapticImpulse(0, amplitude, duration);
         }
     }
 }
